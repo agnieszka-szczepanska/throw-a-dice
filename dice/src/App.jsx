7e8a4fde46f5031352047d3fbfc3fc56d4b9fc5a
Agnieszka Szczepańska <szczepanska.agnieszka01@gmail.com>
2023-01-27 14:04:31 +0100

    add feature of multiple rolls

--- dice/src/App.jsx
+++ dice/src/App.jsx
@@ -5,23 +5,32 @@ import "./App.css";
 function App() {
   const [result, setResult] = useState([]);
   const [inputValue, setInputValue] = useState(1);
-  const [rollResult, setRollResults] = useState(0);
+  const [rollResult, setRollResults] = useState([]);
 
-  const numberOfRolls = (inputValue, roll) => {
+  const numberOfRolls = (max, inputValue) => {
+    let newResult = [];
+    let currentResults = [];
     for (let i = 0; i < inputValue; i++) {
-      roll([i]);
+      newResult.push({
+        dice: `d${max}`,
+        roll: Math.ceil(Math.random() * max),
+      });
+      currentResults.push(newResult.map((e) => e.roll));
+      setRollResults(currentResults[currentResults.length - 1]);
     }
-  };
 
-  const roll = (max) => {
-    setResult([
-      ...result,
-      { dice: `d${max}`, roll: Math.ceil(Math.random() * max), id: uuidv4() },
-    ]);
+    const multipleRollDiceValue = newResult.reduce(
+      (accumulator, value) => accumulator + value.roll,
+      0
+    );
+    newResult = {
+      dice: `d${max}`,
+      roll: multipleRollDiceValue,
+      id: uuidv4(),
+    };
+    newResult = [newResult, ...result];
+    setResult(newResult);
   };
-
-  console.log(inputValue);
-  console.log(result);
 
   const addOne = () => {
     setInputValue(inputValue + 1);
@@ -30,19 +39,25 @@ function App() {
     setInputValue(inputValue - 1);
   };
 
+  console.log("rollResult:", rollResult);
   return (
     <div className="App">
       <div>You can't roll like Elvis</div>
       <div>
         <button onClick={() => numberOfRolls(4, inputValue)}>d4</button>
-        <button onClick={() => roll(6, inputValue)}>d6</button>
-        <button onClick={() => roll(8, inputValue)}>d8</button>
-        <button onClick={() => roll(10, inputValue)}>d10</button>
-        <button onClick={() => roll(12, inputValue)}>d12</button>
-        <button onClick={() => roll(20, inputValue)}>d20</button>
-        <button onClick={() => roll(100, inputValue)}>d100</button>
+        <button onClick={() => numberOfRolls(6, inputValue)}>d6</button>
+        <button onClick={() => numberOfRolls(8, inputValue)}>d8</button>
+        <button onClick={() => numberOfRolls(10, inputValue)}>d10</button>
+        <button onClick={() => numberOfRolls(12, inputValue)}>d12</button>
+        <button onClick={() => numberOfRolls(20, inputValue)}>d20</button>
+        <button onClick={() => numberOfRolls(100, inputValue)}>d100</button>
       </div>
-
+      <div>
+        <p>Results of dice sum</p>
+        {rollResult.reduce((a, b) => a + b, 0)}
+        <p>Results of each dice</p>
+        {rollResult.join(" + ")}
+      </div>
       <label id="rolls">Number of dices</label>
       <div>
         <button onClick={() => deductOne()}>-</button>
@@ -72,5 +87,3 @@ function App() {
 }
 
 export default App;
-
-// {result.length > 0 && `roll:${result[result.length-1].roll}`}
