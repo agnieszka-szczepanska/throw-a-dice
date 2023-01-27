92a25abd1ccc957b538a63522e410d0422065aaf
Agnieszka Szczepańska <szczepanska.agnieszka01@gmail.com>
2023-01-27 14:35:34 +0100

    add styles

--- dice/src/App.jsx
+++ dice/src/App.jsx
@@ -58,7 +58,9 @@ function App() {
   console.log("rollResult:", rollResult);
   return (
     <div className="App">
+      <h1>Throw a dice app</h1>
       <div>You can't roll like Elvis</div>
+      <br />
       <div>
         <button onClick={() => numberOfRolls(4, inputValue)}>d4</button>
         <button onClick={() => numberOfRolls(6, inputValue)}>d6</button>
@@ -68,23 +70,7 @@ function App() {
         <button onClick={() => numberOfRolls(20, inputValue)}>d20</button>
         <button onClick={() => numberOfRolls(100, inputValue)}>d100</button>
       </div>
-      <div>
-        {inputValue === 1 ? (
-          <div>
-            <p>Results of dice sum</p>
-            <div>{rollResult.reduce((a, b) => a + b, 0) + bonusPoints}</div>
-            {bonusPoints > 0 && <div>{` BP: ${bonusPoints} `}</div>}
-          </div>
-        ) : (
-          <div>
-            <p>Results of dice sum</p>
-            {rollResult.reduce((a, b) => a + b, 0) + bonusPoints}
-            <p>Results of each dice</p>
-            <div>{` RR: ${rollResult.join(" + ")} `}</div>
-            <div>{` BP: ${bonusPoints} `}</div>
-          </div>
-        )}
-      </div>
+
       <label id="rolls">Number of dices</label>
       <div>
         <button onClick={() => deductOneDiceRoll()}>-</button>
@@ -111,16 +97,37 @@ function App() {
         />
         <button onClick={() => addOneBonusPoints()}>+</button>
       </div>
-
-      <div>
-        History
-        {result.map((e) => {
-          return (
-            <div key={e.id}>
-              {e.dice} : {e.roll}
+      <br />
+      <div className="resultContainer">
+        <h3>RESULT:</h3>
+        <div>
+          {inputValue === 1 ? (
+            <div>
+              <h4>Results of dice sum:</h4>
+              <div>{rollResult.reduce((a, b) => a + b, 0) + bonusPoints}</div>
+              {bonusPoints > 0 && <div>{` BP: ${bonusPoints} `}</div>}
             </div>
-          );
-        })}
+          ) : (
+            <div>
+              <h4>Results of dice sum:</h4>
+              {rollResult.reduce((a, b) => a + b, 0) + bonusPoints}
+              <h4>Results of each dice:</h4>
+              <div>{` Roll Result: ${rollResult.join(" + ")} `}</div>
+              <div>{` Bonus Points: ${bonusPoints} `}</div>
+              <br />
+            </div>
+          )}
+        </div>
+        <div>
+          <h4>History:</h4>
+          {result.map((e) => {
+            return (
+              <div key={e.id}>
+                {e.dice} : {e.roll}
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
