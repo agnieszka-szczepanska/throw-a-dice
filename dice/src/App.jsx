df10c34818d535cec228c0ef408cbe449ed91be6
Agnieszka Szczepańska <szczepanska.agnieszka01@gmail.com>
2023-01-27 14:01:03 +0100

    add dist roll

--- dice/src/App.jsx
+++ dice/src/App.jsx
@@ -1,34 +1,76 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { v4 as uuidv4 } from "uuid";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [result, setResult] = useState([]);
+  const [inputValue, setInputValue] = useState(1);
+  const [rollResult, setRollResults] = useState(0);
+
+  const numberOfRolls = (inputValue, roll) => {
+    for (let i = 0; i < inputValue; i++) {
+      roll([i]);
+    }
+  };
+
+  const roll = (max) => {
+    setResult([
+      ...result,
+      { dice: `d${max}`, roll: Math.ceil(Math.random() * max), id: uuidv4() },
+    ]);
+  };
+
+  console.log(inputValue);
+  console.log(result);
+
+  const addOne = () => {
+    setInputValue(inputValue + 1);
+  };
+  const deductOne = () => {
+    setInputValue(inputValue - 1);
+  };
 
   return (
     <div className="App">
+      <div>You can't roll like Elvis</div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <button onClick={() => numberOfRolls(4, inputValue)}>d4</button>
+        <button onClick={() => roll(6, inputValue)}>d6</button>
+        <button onClick={() => roll(8, inputValue)}>d8</button>
+        <button onClick={() => roll(10, inputValue)}>d10</button>
+        <button onClick={() => roll(12, inputValue)}>d12</button>
+        <button onClick={() => roll(20, inputValue)}>d20</button>
+        <button onClick={() => roll(100, inputValue)}>d100</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <label id="rolls">Number of dices</label>
+      <div>
+        <button onClick={() => deductOne()}>-</button>
+        <input
+          id="rolls"
+          min={1}
+          value={inputValue}
+          type="number"
+          placeholder="Number of Rolls"
+          step={1}
+        />
+        <button onClick={() => addOne()}>+</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div>
+        History
+        {result.map((e) => {
+          return (
+            <div key={e.id}>
+              {e.dice} : {e.roll}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
 
 export default App;
+
+// {result.length > 0 && `roll:${result[result.length-1].roll}`}
